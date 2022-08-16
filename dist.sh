890d0d9cafbb0eac4e343090ecef2cfb0a385b93
Claudio Bianucci <bianucci.c@gmail.com>
2022-08-16 02:19:22 +0200

    create package for distribution

--- dist.sh
+++ dist.sh
@@ -7,3 +7,6 @@ cp src/manifest.json dist/
 cp -r src/images dist/
 
 echo "var exports = {};$(cat dist/background.js)" >dist/background.js
+
+rm copy-jwt.zip
+zip -r copy-jwt.zip dist/* -x "*.DS_Store"
